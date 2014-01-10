9b0589a953c5f0f10693fd2be700b1f4f86c094d
RadekJ1992 <radoslaw.jarzynka@gmail.com>
2014-01-10 06:54:58 +0100

    zmiany - obsługa wysyłania danych jako ciągu bajtów w postaci heksadecymalnej

--- EncryptWorker/WorkerRole.cs
+++ EncryptWorker/WorkerRole.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Threading;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
@@ -19,11 +20,12 @@ namespace EncryptWorker
     /// </summary>
     public class WorkerRole : RoleEntryPoint
     {
-
         private AutoResetEvent connectionWaitHandle = new AutoResetEvent(false);
 
         StreamReader LoggerReader;
         StreamWriter LoggerWriter;
+
+        List<string> loggerList;
         /// <summary>
         /// Metoda uruchamiaj¹ca workera i rozpoczynaj¹ca nas³uch na odpowiednim IP i porcie
         /// </summary>
@@ -61,6 +63,7 @@ namespace EncryptWorker
         /// <returns>zmienna bitowa informuj¹ca czy start siê uda³</returns>
         public override bool OnStart()
         {
+            loggerList = new List<string>();
             ServicePointManager.DefaultConnectionLimit = 12;
             return base.OnStart();
         }
@@ -79,6 +82,11 @@ namespace EncryptWorker
                 connectionWaitHandle.Set();
 
                 Cryptography crypt = new Cryptography();
+                AesManaged myAes = new AesManaged();
+                String AESKey = "SuperTajnyKlucz1"; //128 bit
+                myAes.Key = Encoding.UTF8.GetBytes(AESKey);
+                String AESIV = "SuperTajnyWektor"; //128 bit
+                myAes.IV = Encoding.UTF8.GetBytes(AESIV);
                 // Po³¹czenie zaakceptowane 
                 Guid clientId = Guid.NewGuid();
                 Trace.WriteLine("Accepted connection with ID " + clientId.ToString(), "Information");
@@ -95,7 +103,15 @@ namespace EncryptWorker
                 {
                     LoggerReader = reader;
                     LoggerWriter = writer;
-                    LoggerWriter.WriteLine("Hello, Logger!\n\r Please wait for incoming connections");
+                    LoggerWriter.WriteLine("Hello, Logger!\n\rPlease wait for incoming connections");
+                    if (loggerList.Count != 0)
+                    {
+                        LoggerWriter.WriteLine("There are some incoming logs");
+                        foreach (string s in loggerList)
+                        {
+                            LoggerWriter.WriteLine(s);
+                        }
+                    }
                     while (true)
                     {
                         try
@@ -120,12 +136,11 @@ namespace EncryptWorker
                 {
                     // losuj klucz szyfru cezara
                     Random rnd = new Random();
-                    int ckey = rnd.Next(1, 128);
                     String key = string.Empty;
                     String decryptedKey = string.Empty;
                     String plainText = string.Empty;
                    // writer.WriteLine("Key: ");
-                    writer.WriteLine(ckey);
+                  //  writer.WriteLine(ckey);
                   //  writer.WriteLine("Welcome to RC4 Encrypting Server!");
                     while (true)
                     {
@@ -137,25 +152,42 @@ namespace EncryptWorker
                             {
                                 //  writer.WriteLine("Please Enter Key:");
                                 key = reader.ReadLine();
+                                //byte[] keyAESBytes = Encoding.UTF8.GetBytes(key);
+                                byte[] keyAESBytes = crypt.StringToByteArray(key);
                                 // odszyfruj klucz
-                                decryptedKey = crypt.Cshift(key, -ckey);
+                                    decryptedKey = crypt.DecryptStringFromBytes_Aes(keyAESBytes, myAes.Key, myAes.IV);
                                 // writer.WriteLine("Please Enter Plain Text:");
+
+                                string isHexStr = reader.ReadLine();
+                                bool isHex = (isHexStr == "hex") ? true : false;
+
                                 plainText = reader.ReadLine();
                                 Console.WriteLine("Key received : {0}", decryptedKey);
                                 Console.WriteLine("Plain Text received : {0}", plainText);
+
                                 byte[] keyBytes = Encoding.ASCII.GetBytes(decryptedKey);
-                                byte[] textBytes = Encoding.ASCII.GetBytes(plainText);
+                                byte[] textBytes;
+                                if (!isHex) textBytes = Encoding.ASCII.GetBytes(plainText);
+                                else textBytes = crypt.StringToByteArray(plainText);
 
                                 Byte[] output = crypt.runRC4(keyBytes, textBytes);
-
+                                // wynik szyfrowania w postaci heksadecymalnej
                                 String outputString = crypt.ByteArrayToString(output);
+                                // wynik szyfrowania w postaci stringu utf8
+                                String outputStringUTF8 = Encoding.UTF8.GetString(output);
 
                                 Byte[] decrypt = crypt.runRC4(keyBytes, output);
 
                                 String decryptString = Encoding.ASCII.GetString(decrypt);
+                                String decryptStringHex = crypt.ByteArrayToString(decrypt);
                                 // wyœlij wynik do loggera
-                                String msg = "From Client : " + clientId.ToString() + "\n\rKey : " + decryptedKey + "\n\rPlain Text : " + plainText + "\n\rResult : " + outputString + "\n\rDecrypt result : " + decryptString + "\n\r";
+                                String msg;
+                                if (!isHex) msg = "From Client : " + clientId.ToString() + "\n\rKey : " + decryptedKey + "\n\rPlain Text : " + plainText + "\n\rResult as hex string : \n\r" 
+                                    + outputString + "\n\rDecrypt result : " + decryptString + "\n\r";
+                                else msg = "From Client : " + clientId.ToString() + "\n\rKey : " + decryptedKey + "\n\rHex string : " + plainText + "\n\rResult as utf8 string : \n\r" 
+                                    + outputStringUTF8 + "\n\rDecrypt hex result : " + decryptStringHex + "\n\r";
                                 if (LoggerWriter != null) LoggerWriter.Write(msg);
+                                else loggerList.Add(msg);
                             }
                             if (command == "quit")
                             {
@@ -184,29 +216,46 @@ namespace EncryptWorker
         /// <summary>
         /// Klasa obs³uguj¹ca szyfrowanie
         /// </summary>
-        public class Cryptography
-        {
+        public class Cryptography {
             /// <summary>
-            /// Metoda szyfruj¹ca z u¿yciem szyfru Cezara
+            /// Metoda deszyfruj¹ca przes³any z wykorzystaniem AES klucz
             /// </summary>
-            /// <param name="str">Szyfrowany tekst</param>
-            /// <param name="shift">Klucz</param>
-            /// <returns>Wynik szyfrowania</returns>
-            public string Cshift(string str, int shift)
-            {
-                string output = null;
-                char[] text = null;
-                text = str.ToCharArray();
-                int temp;
+            /// <param name="cipherText">szyfrogram w postaci tablicy bajtów</param>
+            /// <param name="Key">podany klucz</param>
+            /// <param name="IV">wektor pocz¹tkowy</param>
+            /// <returns>odszyfrowany string</returns>
+        public string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV) {
+            // SprawdŸ argumenty 
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
 
-                for (int i = 0; i < str.Length; i++)
+            string plaintext = null;
+
+            // Tworzenie obiektu AESManaged z danym kluczem i wektorem pocz¹tkowym
+            using (AesManaged aesAlg = new AesManaged()) {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
                 {
-                    temp = (int)(text[i] + shift);
-                    output += (char)temp;
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
                 }
-                return output;
             }
-            /// <summary>
+            return plaintext;
+       }
+         /// <summary>
             /// Metoda szyfruj¹ca z u¿yciem szyfru RC4
             /// </summary>
             /// <param name="keyBytes">Bajty klucza</param>
